19074d6584bd8f2eb36945a2941204a5cb83a81a
Yiyu Qian <88706924+qyy752457002@users.noreply.github.com>
2023-03-11 20:14:15 +1100

    Update main.py

--- Python-Checkers-AI/main.py
+++ Python-Checkers-AI/main.py
@@ -10,69 +10,13 @@ import pandas as pd
 FPS = 60
 
 # WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-# pygame.display.set_caption('Checkers')
-
-def get_row_col_from_mouse(pos):
-    x, y = pos
-    row = y // SQUARE_SIZE
-    col = x // SQUARE_SIZE
-    return row, col
-
-def game(white_traces, red_traces, number):
-    run = True
-    clock = pygame.time.Clock()
-    game = Game(WIN)
-
-    while run:
-        clock.tick(FPS)
-
-        if game.turn == WHITE:
-            white_agent = MCTS_agent(game.get_board(), WHITE)
-            new_board, reward, next_turn, terminate, movement_info = white_agent.get_action()
-
-            white_traces['piece'].append(WHITE)
-            white_traces['current_position'].append(movement_info[1])
-            white_traces['next_position'].append(movement_info[2])
-            white_traces['skip'].append("-") if not movement_info[3] else white_traces['skip'].append(movement_info[3])
-            white_traces['reward'].append(reward)
-
-        else:
-            red_agent = MCTS_agent(game.get_board(), RED)
-            new_board, reward, next_turn, terminate, movement_info = red_agent.get_action()
-
-            red_traces['piece'].append(RED)
-            red_traces['current_position'].append(movement_info[1])
-            red_traces['next_position'].append(movement_info[2])
-            red_traces['skip'].append("-") if not movement_info[3] else red_traces['skip'].append(movement_info[3])
-            red_traces['reward'].append(reward)
-
-        game.ai_move(new_board)
-
-        if game.winner() != None:
-            run = False      
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-
-        game.update()
-    
-    pygame.quit()
-
-    df = pd.DataFrame(white_traces)
-    df.to_csv(f"D:/XAI Process Mining Research/Python-Checkers-AI/RL policy traces/white_episode{number}.csv")
-
-    df = pd.DataFrame(red_traces)
-    df.to_csv(f"D:/XAI Process Mining Research/Python-Checkers-AI/RL policy traces/red_episode{number}.csv")
-
+# pygame.display.set_caption('Checkers')s
 if __name__ == "__main__":
     
-    episodes = 100
+    for i in range(1, 101):
 
-    white_traces = { 'piece': [], 'current_position': [], 'next_position': [], "skip": [], 'reward': [] }
-    red_traces = { 'piece': [], 'current_position': [], 'next_position': [], "skip": [], 'reward': [] }
-
-    for i in range(1, episodes + 1):
+        white_traces = { 'piece id': [], 'move': [], "skip": [], 'reward': [] }
+        red_traces = { 'piece id': [], 'move': [], "skip": [], 'reward': [] }
 
         run = True
 
@@ -84,20 +28,18 @@ if __name__ == "__main__":
                 white_agent = MCTS_agent(game.get_board(), WHITE)
                 new_board, reward, next_turn, terminate, movement_info = white_agent.get_action()
 
-                white_traces['piece'].append(WHITE)
-                white_traces['current_position'].append(movement_info[1])
-                white_traces['next_position'].append(movement_info[2])
-                white_traces['skip'].append("-") if not movement_info[3] else white_traces['skip'].append(movement_info[3])
+                white_traces['piece id'].append(movement_info[0])
+                white_traces['move'].append(movement_info[1])
+                white_traces['skip'].append(movement_info[2])
                 white_traces['reward'].append(reward)
 
             else:
                 red_agent = MCTS_agent(game.get_board(), RED)
                 new_board, reward, next_turn, terminate, movement_info = red_agent.get_action()
 
-                red_traces['piece'].append(RED)
-                red_traces['current_position'].append(movement_info[1])
-                red_traces['next_position'].append(movement_info[2])
-                red_traces['skip'].append("-") if not movement_info[3] else red_traces['skip'].append(movement_info[3])
+                red_traces['piece id'].append(movement_info[0])
+                red_traces['move'].append(movement_info[1])
+                red_traces['skip'].append(movement_info[2])
                 red_traces['reward'].append(reward)
 
             game.ai_move(new_board)
@@ -112,6 +54,3 @@ if __name__ == "__main__":
 
         df = pd.DataFrame(red_traces)
         df.to_csv(f"D:/XAI Process Mining Research/Python-Checkers-AI/RL policy traces/red_episode{i}.csv")
-
-
-    
